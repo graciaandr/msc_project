7d0acab32594e45d69295842f3a88ed114c805fb
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-03-17 12:09:13 +0100

    subsetted rrbs data set and adjusted py script

--- scratch/read_data.py
+++ scratch/read_data.py
@@ -3,7 +3,10 @@ import csv
 import numpy as np
 
 # took example rrbs data from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM4830470
-df = pd.read_csv('3032_S29_R1_001.CGmap', sep = '\t', header=None)
+df = pd.read_csv('scratch/3032_S29_R1_001.CGmap', sep = '\t', header=None)
+
+# small test data of 500ish lines
+df = pd.read_csv('scratch/subsetted_rrbs_data.txt', sep = '\t', header=None)
 
 # columns according to https://cgmaptools.github.io/cgmaptools_documentation/file-formats.html
 df.columns = ['Chr', 'Nuc', 'Pos', 'Context', 'DiNuc', 'MethyLevel', 'MC', 'NC']
