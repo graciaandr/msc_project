0261c06eab0818270bca27e9afd6f45202e15df0
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-04-11 11:41:03 +0100

    Update read_data.py

--- implementation/read_data.py
+++ implementation/read_data.py
@@ -3,7 +3,7 @@ import csv
 import numpy as np
 
 # took example rrbs data from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE104998
-df = pd.read_csv('data/GSM2813719_YB5_DMSO4d1.txt', sep = '\t', header=None)
+df = pd.read_csv('../data/YB5_CRC_study/GSM2813719_YB5_DMSO4d1.txt', sep = '\t')
 
 # # columns according to https://cgmaptools.github.io/cgmaptools_documentation/file-formats.html
 # df.columns = ['Chr', 'Nuc', 'Pos', 'Context', 'DiNuc', 'MethyLevel', 'MC', 'NC']
