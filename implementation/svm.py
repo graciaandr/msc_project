21c7a138228093dccaf3c35d2e3817c2e4acf4d7
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-04-12 11:42:27 +0100

    Update svm.py

--- implementation/svm.py
+++ implementation/svm.py
@@ -22,11 +22,11 @@ df8 = pd.read_csv('../data/YB5_CRC_study/GSM2813726_YB5_con2.txt', sep = '\t')
 
 # add column with labels (0,1) for control and treated samples
 # df_trt = pd.concat([df1, df2, df3, df4, df5, df6])
-df_trt = df1 # .head(1000)
+df_trt = (df1.head(50000)).copy()
 df_trt['label'] = 1
 
 # df_ctrl = pd.concat([df7, df8])
-df_ctrl = df7 # .head(1000)
+df_ctrl = (df7.head(50000)).copy()
 df_ctrl['label'] = 0
 
 # merge trt and ctrl data frames
@@ -36,28 +36,34 @@ print(df.shape)
 
 # keep numeric values - for classification 
 df_num = df[['coverage', 'freqC', 'freqT', 'label']]
-print(df_num.head(5))
-print(df_num.tail(5))
 
 X = df[['coverage', 'freqC', 'freqT']]
 y = df_num['label']
 
+print('here')
+
 # split data into training and testing data set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+print('split data into training and testing data')
 
-# initialize
+# initialize and train SVM classifier
 clf = svm.SVC()
 clf.fit(X_train, y_train)
+print('classifier has been trained')
 
+# apply SVM to test data
 y_pred = clf.predict(X_test)
+
+# return accuracy and precision score
 print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 print("Precision:", metrics.precision_score(y_test, y_pred))
 
-# Plot confusion matrix (source: https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea)
+# calculate and plot confusion matrix (source: https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea)
 cf_matrix = metrics.confusion_matrix(y_test, y_pred)
-sns.heatmap(cf_matrix, annot=True)
+sns.heatmap(cf_matrix, annot=True, fmt='.3g')
 plt.show()
 
+# cf matrix with percentages
 sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, 
             fmt='.2%', cmap='Blues')
 plt.show()
