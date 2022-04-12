4351c9c52b0a7f68011de741c64c085bf899c384
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-04-12 16:01:33 +0100

    starting to use methylkit

--- implementation/differential_methylation_analysis.R
+++ implementation/differential_methylation_analysis.R
@@ -1,11 +1,32 @@
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
+# if (!require("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
 
-BiocManager::install("methylKit")
+setwd('C:/Users/andri/Documents/Uni London/QMUL/SemesterB/Masters_project/msc_project/data/YB5_CRC_study/')
 
-install.packages( c("data.table", "mixtools", "devtools"))
-source("http://bioconductor.org/biocLite.R")
-biocLite(c("GenomicRanges","IRanges"))
-# install from github
+library(methylKit)
+library(GenomicRanges)
+library(IRanges)
 library(devtools)
-install_github("ShengLi/edmr")
+# install_github("ShengLi/edmr")
+library(edmr)
+
+# file.list=list( system.file("extdata", 
+#                             "GSM2813719_YB5_DMSO4d1.txt", package = "methylKit"),
+#                 system.file("extdata",
+#                             "GSM2813720_YB5_DMSO4d2.txt", package = "methylKit"),
+#                 system.file("extdata", 
+#                             "GSM2813725_YB5_con1.txt", package = "methylKit"),
+#                 system.file("extdata", 
+#                             "GSM2813726_YB5_con2.txt", package = "methylKit") )
+
+file.list = list.files(path = "C:/Users/andri/Documents/Uni London/QMUL/SemesterB/Masters_project/msc_project/data/YB5_CRC_study/", pattern= 'GSM')
+
+file.list = system.file(file.list, package = "methylKit")
+myobj=methRead(file.list,
+               sample.id=list("test1","test2", "test3","test4","test5","test6","ctrl1","ctrl2"),
+               assembly="hg18",
+               treatment=c(1,1,0,0),
+               context="CpG"
+)
+
+getMethylationStats(myobj[[2]],plot=FALSE,both.strands=FALSE)
