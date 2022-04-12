267a0a60d58fd27fb9a12969a229cb1d9aa7a215
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-04-12 13:49:38 +0100

    Update kmeans.py

--- implementation/kmeans.py
+++ implementation/kmeans.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -24,45 +25,65 @@ df8 = pd.read_csv('../data/YB5_CRC_study/GSM2813726_YB5_con2.txt', sep = '\t')
 
 # add column with labels (0,1) for control and treated samples
 # df_trt = pd.concat([df1, df2, df3, df4, df5, df6])
-df_trt = df1 # .head(1000)
-df_trt['label'] = 1
+df_trt = (df1.head(10000)).copy()
+df_trt['class'] = 1
 
 # df_ctrl = pd.concat([df7, df8])
-df_ctrl = df7 # .head(1000)
-df_ctrl['label'] = 0
+df_ctrl = (df7.head(10000)).copy()
+df_ctrl['class'] = 0
 
 # merge trt and ctrl data frames
 df = pd.concat([df_trt, df_ctrl])
 print(df.shape)
 
-X = df[['coverage', 'freqC', 'freqT']]
+df = df[['coverage', 'freqC', 'freqT', 'class']]
+print(df.head(5))
 
-scaler = StandardScaler()
-scaled_features = scaler.fit_transform(X)
+plt.scatter(df.freqC, df.freqT, alpha = 0.6,  s=df.coverage)
+plt.show()
+# scaler = StandardScaler()
+# scaled_features = scaler.fit_transform(df)
+# print('scaled the features')
+# print(scaled_features)
 
 kmeans = KMeans(
     init="random",
-    n_clusters=3,
-    n_init=10,
-    max_iter=300,
+    n_clusters=4,
+    n_init=15,
+    max_iter=1000,
     random_state=42
 )
 
-kmeans.fit(scaled_features)
+# kmeans.fit(scaled_features)
+label = kmeans.fit_predict(df)
+df['cluster'] = label
+print('clustering done')
+print(df.head(5))
 
-# The lowest SSE value
-kmeans.inertia_
+pred_label = df['cluster']
+real_label = df['class']
+cf_matrix = metrics.confusion_matrix(real_label, pred_label)
+sns.heatmap(cf_matrix, annot=True, fmt='.3g')
+plt.show()
 
-# Final locations of the centroid
-kmeans.cluster_centers_
+#plotting the results
+plt.scatter(df.freqC, df.freqT, c=df.cluster, alpha = 0.6,  s=df.coverage)
+plt.show()
+
+
+# # The lowest SSE value
+# print(kmeans.inertia_)
+
+# # Final locations of the centroid
+# print(kmeans.cluster_centers_)
 
 # The number of iterations required to converge
-kmeans.n_iter_
+# print('n_iter: ', kmeans.n_iter_)
 
 kmeans_kwargs = {
     "init": "random",
     "n_init": 10,
-    "max_iter": 300,
+    "max_iter": 1000,
     "random_state": 42,
 }
 
@@ -70,7 +91,7 @@ kmeans_kwargs = {
 sse = []
 for k in range(1, 11):
     kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
-    kmeans.fit(scaled_features)
+    kmeans.fit(df)
     sse.append(kmeans.inertia_)
     
 plt.style.use("fivethirtyeight")
@@ -80,25 +101,25 @@ plt.xlabel("Number of Clusters")
 plt.ylabel("SSE")
 plt.show()
 
-# kl = KneeLocator(
-#     range(1, 11), sse, curve="convex", direction="decreasing"
-# )
+kl = KneeLocator(
+    range(1, 11), sse, curve="convex", direction="decreasing"
+)
 
-# kl.elbow
+print('elbow: ', kl.elbow)
 
-# # A list holds the silhouette coefficients for each k
-# silhouette_coefficients = []
+# A list holds the silhouette coefficients for each k
+silhouette_coefficients = []
 
-# # Notice you start at 2 clusters for silhouette coefficient
-# for k in range(2, 11):
-#     kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
-#     kmeans.fit(scaled_features)
-#     score = silhouette_score(scaled_features, kmeans.labels_)
-#     silhouette_coefficients.append(score)
+# Notice you start at 2 clusters for silhouette coefficient
+for k in range(2, 11):
+    kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
+    kmeans.fit(df)
+    score = silhouette_score(df, kmeans.labels_)
+    silhouette_coefficients.append(score)
     
-# plt.style.use("fivethirtyeight")
-# plt.plot(range(2, 11), silhouette_coefficients)
-# plt.xticks(range(2, 11))
-# plt.xlabel("Number of Clusters")
-# plt.ylabel("Silhouette Coefficient")
-# plt.show()
+plt.style.use("fivethirtyeight")
+plt.plot(range(2, 11), silhouette_coefficients)
+plt.xticks(range(2, 11))
+plt.xlabel("Number of Clusters")
+plt.ylabel("Silhouette Coefficient")
+plt.show()
