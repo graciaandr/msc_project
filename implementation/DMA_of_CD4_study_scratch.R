a5fde75b05b049b862e2ce4ea597c3b77786e50a
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-04-29 11:54:22 +0100

    Update DMA_of_CD4_study_scratch.R

--- implementation/DMA_of_CD4_study_scratch.R
+++ implementation/DMA_of_CD4_study_scratch.R
@@ -23,7 +23,7 @@ setwd("C:/Users/andri/Documents/Uni London/QMUL/SemesterB/Masters_project/msc_pr
 ## create file list
 file.list = list.files(path = "C:/Users/andri/Documents/Uni London/QMUL/SemesterB/Masters_project/msc_project/data/CD4_Tcell_study", pattern= '*.txt$')
 list_of_files = as.list(file.list)
-list_of_files[c(2,5,6,13,14)] = NULL
+list_of_files[c(2,5,6,11,13,14)] = NULL
   # remove ctrl1, ctrl4 and ctrl7 as cluster indicated bas results for those, 
   # accordingly also removed one case samples (after clustering and PCA, decided to remove case2 & 4 - case 7 is too much)
 print(list_of_files)
@@ -35,7 +35,7 @@ myobj=methRead(location = list_of_files,
                # sample.id =list("ctrl2","ctrl3","ctrl5","ctrl6","case1","case3","case5", "case6"),
                sample.id =list("ctrl1","ctrl3","ctrl4","ctrl5","case1","case2","case3","case5"),
                assembly ="hg38", # study used GrCh38 - hg38
-               treatment = c(0,0,0,1,1,1,1),
+               treatment = c(0,0,0,0,1,1,1,1),
                context="CpG",
                header = TRUE, 
                pipeline = 'bismarkCytosineReport',
@@ -63,7 +63,8 @@ time.taken
 
 
 # merge samples
-meth=unite(myobj, destrand=FALSE, min.per.group = 2L) ## adjust min per group to see if i get more cpgs eventually 
+# meth=unite(myobj, destrand=FALSE, min.per.group = 2L) ## adjust min per group to see if i get more cpgs eventually 
+meth=unite(myobj, destrand=FALSE) ## adjust min per group to see if i get more cpgs eventually 
   # check parameter 'min.per.group' (want cpg in ALL samples incld. case/ctrl) -- no missing values since small pilot study
   # By default only regions/bases that are covered in all samples are united as methylBase object -- according to https://www.rdocumentation.org/packages/methylKit/versions/0.99.2/topics/unite
 head(meth)
@@ -71,7 +72,6 @@ nrow(data.frame(meth))
 
 # cluster samples
 clusterSamples(meth, dist="correlation", method="ward.D2", plot=TRUE) 
-# the plot shows that ctrl1 and 4 were trash --> replace with other ctrls --> ctrl7 still problem though
 
 # pca plots
 # PCASamples(meth, screeplot=TRUE)
