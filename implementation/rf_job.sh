6c3db55153ecdebd69641da80d45dd55ff155bad
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-05-11 12:01:03 +0100

    Update rf_job.sh

--- implementation/rf_job.sh
+++ implementation/rf_job.sh
@@ -3,7 +3,7 @@
 #$ -j y
 #$ -pe smp 4
 #$ -l h_rt=240:0:0
-#$ -l h_vmem=8G
+#$ -l h_vmem=16G
 #$ -N random_forest_job
 #$ -m bea
 
@@ -13,3 +13,4 @@ source msc_pyenv/bin/activate
 
 # run SVM python script
 python python_code/random_forest.py
+
