4b29dfcf9cf8362c4a7365a3b93b07e62b6a8588
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-05-11 15:23:05 +0100

    feature selection and enhanced roc curve for RF classifier

--- implementation/random_forest.py
+++ implementation/random_forest.py
@@ -54,7 +54,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_
 # print(df.isnull().sum())
 
 # initialize and train SVM classifier
-clf = RandomForestClassifier(max_depth=20, random_state=0)
+clf = RandomForestClassifier(max_depth=100, random_state=150)
 fit = clf.fit(X_train, y_train)
 
 # apply SVM to test data
@@ -63,28 +63,34 @@ y_pred = fit.predict(X_test)
 # return accuracy and precision score
 print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 print("Precision:", metrics.precision_score(y_test, y_pred))
+print("AUC-ROC Score:", metrics.roc_auc_score(y_test, y_pred))
+print(metrics.classification_report(y_test, y_pred))
+
+metrics.plot_roc_curve(clf, X_test, y_test) 
+plt.show()
 
 # calculate and plot confusion matrix (source: https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea)
-# cf_matrix = metrics.confusion_matrix(y_test, y_pred)
-# sns.heatmap(cf_matrix, annot=True, fmt='.3g')
-# plt.show()
+cf_matrix = metrics.confusion_matrix(y_test, y_pred)
+sns.heatmap(cf_matrix, annot=True, fmt='.3g')
+plt.show()
 
-# # cf matrix with percentages
-# sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, 
-#             fmt='.2%', cmap='Blues')
-# plt.show()
+# cf matrix with percentages
+sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, 
+            fmt='.2%', cmap='Blues')
+plt.show()
 
 # feature selection 
-print("Num Features: %s" % (fit.n_features_))
+print("Num Features: %s" % (fit.n_features_in_))
 features = list(df.columns)
 
 f_i = list(zip(features,clf.feature_importances_))
 f_i.sort(key = lambda x : x[1])
-f_i = f_i[-50:]
+# f_i = f_i[-50:]
+f_i = f_i[-45:]
 
 # print(f_i[-10:])
-# plt.barh([x[0] for x in f_i],[x[1] for x in f_i])
-# plt.show()
+plt.barh([x[0] for x in f_i],[x[1] for x in f_i])
+plt.show()
 
 # print(f_i)
 
@@ -115,6 +121,11 @@ y_pred = fit.predict(X_test)
 # return accuracy and precision score
 print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 print("Precision:", metrics.precision_score(y_test, y_pred))
+print("AUC-ROC Score:", metrics.roc_auc_score(y_test, y_pred))
+print(metrics.classification_report(y_test, y_pred))
+
+metrics.RocCurveDisplay.from_estimator(clf, X_test, y_test)
+plt.show()
 
 ## calculate and plot confusion matrix (source: https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea)
 cf_matrix = metrics.confusion_matrix(y_test, y_pred)
