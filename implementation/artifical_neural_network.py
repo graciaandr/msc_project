9e77c6a7fabdaeb98aca6457636561db351b99b5
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-05-17 11:02:30 +0100

    retrain ANN with the selected features

--- implementation/artifical_neural_network.py
+++ implementation/artifical_neural_network.py
@@ -128,7 +128,7 @@ plt.plot(np.array(range(epochs_ran // log_inteval)) * log_inteval + log_inteval,
 plt.ylim([0, 1])
 plt.title("Learning curve")
 plt.legend()
-plt.savefig('../scratch/NN_Learning_Curve.png')
+plt.savefig('../scratch/NN_Learning_Curve_all_features.png')
 plt.close()
 
 ## calculate accuracy
@@ -192,10 +192,138 @@ data = pd.DataFrame({
 
 data.sort_values("mean_abs_shap", ascending=False)[:10]
 
-print(data['mean_abs_shap'])
+print(data[['mean_abs_shap', 'name']])
 shap.summary_plot(shap_values, features=x_samples, feature_names=features)
 plt.savefig('../scratch/feature_selection_ANN.png')
+plt.show()
 plt.close()
+
+
+### Neural Network with selected features
+# assign X matrix (numeric values to be clustered) and y vector (labels) 
+features = list(data.name)
+f_i = list(zip(features,data.mean_abs_shap))
+f_i.sort(key = lambda x : x[1])
+f_i = f_i[-30:]
+first_tuple_elements = []
+for a_tuple in f_i:
+	first_tuple_elements.append(a_tuple[0])
+first_tuple_elements.append('label')
+
+# subset of data frame that only includes the n selected features
+df_selected = df[first_tuple_elements]
+# assign X matrix (numeric values to be clustered) and y vector (labels) 
+df_X = df_selected.drop(['label'], axis=1)
+df_y = df_selected.loc[:, 'label']
+
+# convert to arrays --> nn model requires arrays as inputs
+X = df_X.to_numpy()
+y = df_y.to_numpy()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6, random_state=21)
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.4, random_state=80)
+
+# print(df.dtypes)
+
+features = df_X.columns
+num_epochs = 5000
+log_inteval = 250
+total_losses = []
+total_val_losses = []
+lr = 1e-4
+lr_decay_inteval = 2500
+lr_decay_rate = 0.3
+
+
+model = nn.Sequential(
+    nn.Linear(len(features), 80),
+    nn.ReLU(),
+    nn.Dropout(0.3),
+    nn.Linear(80, 256),
+    nn.ReLU(),
+    nn.Dropout(0.6),
+    nn.Linear(256, 1),
+)
+
+loss_fn = torch.nn.BCELoss()
+opt = optim.Adam(model.parameters(), lr=lr)
+
+def init_normal(m):
+    if type(m) == nn.Linear:
+        nn.init.xavier_normal_(m.weight, 0.06)
+
+model.apply(init_normal)
+
+for epoch in range(1, num_epochs+1):
+    y_pred = model(torch.tensor(X_train, dtype=torch.float))
+    y_pred = torch.sigmoid(y_pred)
+    opt.zero_grad()
+    loss = loss_fn(y_pred[:, 0], torch.tensor(y_train, dtype=torch.float))
+    loss.backward()
+    opt.step()
+    total_losses.append(loss.item())
+    if epoch % log_inteval == 0: # Logging
+        epochs_ran = epoch
+        model.eval()
+        with torch.no_grad():
+            y_pred = model(torch.tensor(X_test, dtype=torch.float))
+            y_pred = torch.sigmoid(y_pred)
+            val_loss = loss_fn(y_pred[:, 0], torch.tensor(y_test, dtype=torch.float))
+            total_val_losses.append(val_loss.item())
+        model.train()
+        print(f"total loss in epoch {epoch} = {'%.4f'%loss}, validation loss = {'%.4f'%val_loss}, lr = {'%.2e'%lr}")
+        if len(total_val_losses) > 3 and val_loss.item() > total_val_losses[-2] and val_loss.item() > total_val_losses[-3]:
+            print(f"Validation loss not improving for {log_inteval * 2} epochs, stopping...")
+            break
+    if epoch % lr_decay_inteval == 0: # Learning rate decay
+        lr *= lr_decay_rate
+        for param_group in opt.param_groups:
+            param_group['lr'] = lr
+
+## plot
+plt.plot(total_losses, 'b', label="train")
+plt.plot(np.array(range(epochs_ran // log_inteval)) * log_inteval + log_inteval, total_val_losses, 'r', label="valid")
+plt.ylim([0, 1])
+plt.title("Learning curve")
+plt.legend()
+plt.savefig('../scratch/NN_Learning_Curve_selected_features.png')
+plt.close()
+
+## calculate accuracy
+# from sklearn.metrics import confusion_matrix
+with torch.no_grad():
+    model.eval()
+    y_pred = model(torch.tensor(X_test, dtype=torch.float))
+    y_pred_lbl = np.where(y_pred.numpy() > 0, 1, 0)
+cf_matrix = metrics.confusion_matrix(y_test, y_pred_lbl)
+cfm = pd.DataFrame(cf_matrix, columns=["T", "F"], index=["P", "N"])
+print(cfm)
+
+# return accuracy and precision score
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred_lbl))
+print("Precision:", metrics.precision_score(y_test, y_pred_lbl))
+print("Recall:", metrics.recall_score(y_test, y_pred_lbl))
+print("F1 Score:", metrics.f1_score(y_test, y_pred_lbl))
+
+specificity1 = cf_matrix[0,0]/(cf_matrix[0,0]+cf_matrix[0,1])
+print('Specificity: ', specificity1 )
+
+sensitivity1 = cf_matrix[1,1]/(cf_matrix[1,0]+cf_matrix[1,1])
+print('Sensitivity: ', sensitivity1)
+
+print(metrics.classification_report(y_test, y_pred_lbl))
+
+sns.heatmap(cf_matrix, annot=True, fmt='.3g')
+plt.savefig('../scratch/cf_matrix_NN_sel_features.png')
+plt.close()
+# plt.show()
+
+# cf matrix with percentages
+sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, 
+            fmt='.2%', cmap='Blues')
+plt.savefig('../scratch/cf_matrix_percentages_NN_sel_features.png')
+plt.close()
+# plt.show()
 
 
 ## validation set ?
