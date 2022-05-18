03821421c4b36c49b1162f6c1722c024721a69a0
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-05-18 14:53:40 +0100

    kmeans clustering for beta value data from CLL study

--- implementation/kmeans.py
+++ implementation/kmeans.py
@@ -9,44 +9,65 @@ from sklearn.preprocessing import StandardScaler
 import seaborn as sns
 import matplotlib.pyplot as plt
 from kneed import KneeLocator
-
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import GridSearchCV
+from imblearn.over_sampling import SMOTE
 
 # load data sets
-# took example rrbs data from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE104998
-df1 = pd.read_csv('../data/YB5_CRC_study/GSM2813719_YB5_DMSO4d1.txt', sep = '\t')
-df2 = pd.read_csv('../data/YB5_CRC_study/GSM2813720_YB5_DMSO4d2.txt', sep = '\t')
-df3 = pd.read_csv('../data/YB5_CRC_study/GSM2813721_YB5_DMSO4d3.txt', sep = '\t')
+df_beta_values = pd.read_csv('../data/classifying_data/CLL_study_filt-beta-values.txt', sep = ';')
+# df_beta_values = pd.read_csv('./classifying_data/CLL_study_filt-beta-values.txt', sep = ';')
 
-df4 = pd.read_csv('../data/YB5_CRC_study/GSM2813722_YB5_HH1_10uM4d1.txt', sep = '\t')
-df5 = pd.read_csv('../data/YB5_CRC_study/GSM2813723_YB5_HH1_10uM4d2.txt', sep = '\t')
-df6 = pd.read_csv('../data/YB5_CRC_study/GSM2813724_YB5_HH1_10uM4d3.txt', sep = '\t')
+# transpose data matrix 
+df_beta_transposed = df_beta_values.transpose() 
+df_beta_transposed.index.name = 'old_column_name' ## this is to make filtering easier later
+df_beta_transposed.reset_index(inplace=True)
 
-df7 = pd.read_csv('../data/YB5_CRC_study/GSM2813725_YB5_con1.txt', sep = '\t')
-df8 = pd.read_csv('../data/YB5_CRC_study/GSM2813726_YB5_con2.txt', sep = '\t')
+# try imputing with several imputation methods
+# impute ctrls with ctrls and cases with cases
+imputer = SimpleImputer(missing_values = np.nan, strategy ='constant', fill_value=50)
+ 
+# extract and add column with labels (0,1) for control and treated samples
+df_ctrl = df_beta_transposed.loc[lambda x: x['old_column_name'].str.contains(r'(ctrl)')]
+df_ctrl = df_ctrl.drop(['old_column_name'], axis=1)
+imputer1 = imputer.fit(df_ctrl)
+imputed_df_ctrl = imputer1.transform(df_ctrl)
+df_ctrl_new = pd.DataFrame(imputed_df_ctrl, columns = df_ctrl.columns)
+df_ctrl_new.loc[:, 'label'] = 0
 
-# add column with labels (0,1) for control and treated samples
-# df_trt = pd.concat([df1, df2, df3, df4, df5, df6])
-df_trt = (df1.head(10000)).copy()
-df_trt['label'] = 1
+df_trt = df_beta_transposed.loc[lambda x: x['old_column_name'].str.contains(r'(case)')]
+df_trt = df_trt.drop(['old_column_name'], axis=1)
+imputer2 = imputer.fit(df_trt)
+imputed_df_trt = imputer2.transform(df_trt)
+df_trt_new = pd.DataFrame(imputed_df_trt, columns = df_trt.columns)
+df_trt_new.loc[:, 'label'] = 1
 
-# df_ctrl = pd.concat([df7, df8])
-df_ctrl = (df7.head(10000)).copy()
-df_ctrl['label'] = 0
+# merge trt and ctrl data frames
+df = pd.concat([df_trt_new, df_ctrl_new])
+
+
+# Resampling the minority class. The strategy can be changed as required. (source: https://www.analyticsvidhya.com/blog/2021/06/5-techniques-to-handle-imbalanced-data-for-a-classification-problem/)
+sm = SMOTE(sampling_strategy='minority', random_state=42)
+# Fit the model to generate the data.
+labels = df.loc[:,'label']
+print('hier1')
+
+data = df.drop('label', axis=1, inplace=False)
+
+print(data.columns)
+print(labels)
+print('hier2')
+oversampled_X, oversampled_Y = sm.fit_resample(data, labels)
+df = pd.concat([pd.DataFrame(oversampled_Y), pd.DataFrame(oversampled_X)], axis=1)
+df = df.apply(pd.to_numeric)
+print(df.shape)
+
 
 # merge trt and ctrl data frames
 df = pd.concat([df_trt, df_ctrl])
+df.drop('label', axis=1)
 print(df.shape)
 
-df = df[['coverage', 'freqC', 'freqT', 'label']]
-print(df.head(5))
-
-# plt.scatter(df.freqC, df.freqT, alpha = 0.6,  s=df.coverage)
-# plt.show()
-
-# scaler = StandardScaler()
-# scaled_features = scaler.fit_transform(df)
-# print('scaled the features')
-# print(scaled_features)
+stop1
 
 kmeans = KMeans(
     init="random",
