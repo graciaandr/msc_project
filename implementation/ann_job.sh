cf145bd6fd39eaf7ea8187ab7f434dac0202ef7d
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-05-24 11:23:01 +0100

    Create ann_job.sh

---
+++ implementation/ann_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -pe smp 4
+#$ -l h_rt=240:0:0
+#$ -l h_vmem=8G
+#$ -N neural_network_job
+#$ -m bea
+
+# load Python
+module load python
+source msc_pyenv/bin/activate
+
+# run SVM python script
+python python_code/artifical_neural_network.py
