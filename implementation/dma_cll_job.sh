d1fb65d8cf238c7a71b32ee53135ea535c54b5f5
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-05-25 15:37:08 +0100

    update job scripts

--- implementation/dma_cll_job.sh
+++ implementation/dma_cll_job.sh
@@ -3,12 +3,13 @@
 #$ -j y
 #$ -pe smp 4
 #$ -l h_rt=240:0:0
-#$ -l h_vmem=8G
+#$ -l h_vmem=32G
+#$ -l highmem
 #$ -N CLL_DMA_job
 #$ -m bea
 
 # load R
 module load R/3.6.1
 
-# run R script that did DMA for CD4 t cell study 
+# run R script that did DMA for CLL study with 451 samples 
 Rscript DMA_of_CLL_study.R
