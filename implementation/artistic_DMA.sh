a8db7ac0d86477bafb8cf699d7e28f9c81f585da
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-05-27 11:33:25 +0100

    Create artistic_DMA.sh

---
+++ implementation/artistic_DMA.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -pe smp 4
+#$ -l h_rt=240:0:0
+#$ -l h_vmem=50G
+#$ -l highmem
+#$ -N job_artistic_DMA
+#$ -m bea
+
+# load R
+module load R/3.6.1
+
+# run R script that did DMA for CLL study with 451 samples 
+Rscript DMA_of_artistic_study.R
