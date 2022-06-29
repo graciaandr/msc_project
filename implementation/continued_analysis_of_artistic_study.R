bdf0196b512bd1d7b0e731f195cfe5649ced06d3
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-06-29 14:55:34 +0100

    corrected removing of droplist CpGs & beta value filtering

--- implementation/continued_analysis_of_artistic_study.R
+++ implementation/continued_analysis_of_artistic_study.R
@@ -16,6 +16,7 @@ library(dplyr)
 ### chr, start, strand, number of cytosines (methylated bases) , number of thymines (unmethylated bases),context and trinucletide context format
 
 setwd("/data/home/bt211038/msc_project/")
+# setwd("C:/Users/andri/Documents/Uni London/QMUL/SemesterB/Masters_project/msc_project/")
 
 
 metadata = read.csv("artistic_trial/Masterfile_groups-MSc-project.csv", sep = ";")
@@ -25,11 +26,14 @@ treatments = as.vector(as.numeric(as.factor(metadata$CIN.type)))
 covariates = data.frame(hpv = as.factor(metadata$HPV.type), age = as.numeric(metadata$age))
 print(metadata %>% head(5))
 
-df_meth <- read.table("classifying_data/artistic_study_df_meth.txt.gz", header=T, quote="\"", sep=";")
+meth <- readRDS("artistic_trial/df_meth.rds")
 print("df meth has this many rows:")
-print(nrow(df_meth))
+print(nrow(meth))
 
-myDiff <- readRDS("artistic_trial/calculateDiffMeth_object.txt")
+df_meth <- methylKit::getData(meth)
+
+myDiff <- readRDS("artistic_trial/calculateDiffMeth_object.rds")
+print("myDiff has this many rows:")
 print(nrow(myDiff))
 
 df_beta_vals <- read.table("classifying_data/artistic_study_initial_beta_values.txt", header=T, sep=";")
@@ -37,10 +41,6 @@ df_beta_vals <- read.table("classifying_data/artistic_study_initial_beta_values.
 # df_beta_vals = df_beta_vals %>% dplyr::filter(chrom == "MT")
 print(head(df_beta_vals))
 
-# ## Actual methylation values for each samples:
-# mat = methylKit::percMethylation(df_meth, rowids = TRUE )
-# beta_values = mat/100
-# head(beta_values)
 
 ## try thresholds of 10%, 25% and 50% for cpgs to keep per conditions
 ## Removing NAs: function to keep rows with n many NAs in that row -- default n is 0.25 
@@ -66,55 +66,65 @@ keep_rows <- function(df, metadata,  perc = 0.25) {
 }
 
 
+### remove droplist CpGs
+df_bed_file <- as.data.frame(read.table("bed_file/hg19-blacklist.v2.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote=""))
+colnames(df_bed_file) <- c("chromosome", "start", "end", "info")
+head(df_bed_file)
+df_dmrs_false_cpgs = NULL
+
+
+### filter droplist CpGs from CpGs and calcDiff object (myDiff)
+for (i in (1:length(df_bed_file$start))) {
+  df_tmp = df_beta_vals %>%
+    dplyr::filter(pos >= df_bed_file$start[[i]] & pos <= df_bed_file$end[[i]] & chrom != df_bed_file$chromosome[[i]])
+  df_dmrs_false_cpgs = rbind(df_dmrs_false_cpgs, df_tmp)
+}
+
+## retrieve the valid CpGs
+df_valid_cpgs = setdiff(df_beta_vals,df_dmrs_false_cpgs)
+print( nrow(df_valid_cpgs))
+
+
+
+### filter droplist CpGs from calcDiff object (myDiff)
+for (i in (1:length(df_bed_file$start))) {
+  df_tmp = myDiff %>%
+    dplyr::filter(start >= df_bed_file$start[[i]] & end <= df_bed_file$end[[i]] & chr != df_bed_file$chromosome[[i]])
+  df_dmrs_false_DMCs = rbind(df_dmrs_false_dmcs, df_tmp)
+}
+
+## retrieve the valid DMCs
+df_valid_DMCs = setdiff(myDiff,df_dmrs_false_DMCs)
+print( nrow(df_valid_DMCs))
+
 # remove all unnecessary rows
-indeces_to_keep = keep_rows(df = df_beta_vals, metadata, perc = 0.5)
-df_beta_vals_filt = df_beta_vals[indeces_to_keep,]
+indeces_to_keep = keep_rows(df = df_valid_cpgs, metadata, perc = 0.5)
+df_beta_vals_filt = df_valid_cpgs[indeces_to_keep,]
 df_meth_new =  df_meth[indeces_to_keep,]
-myDiff2 = myDiff[indeces_to_keep,]
-
-print("#Rows of df beta vals after NA handeling: ")
-print(nrow(df_beta_vals_filt))
-write.table(df_beta_vals_filt,
-             file = "/data/home/bt211038/msc_project/classifying_data/artistic_study_betas_b4_EDMR_50threshold.txt",
-             col.names = TRUE, sep = ";", row.names = TRUE)
-
+myDiff2 = df_valid_DMCs[indeces_to_keep,]
 
 ## Q Value adjustment
 adj_q_vals = p.adjust(myDiff2$pvalue, method = "BH")
 myDiff2$qvalue = adj_q_vals
-df_adjusted_diff_meth = myDiff2
+
+# filter myDiff for qvalue of 0.05 and methylationDifference of 10 
+df_adjusted_diff_meth = methylKit::getMethylDiff(qvalue = 0.05, difference = 10)
+
+
+print("#Rows of df beta vals after NA handeling: ")
+print(nrow(df_beta_vals_filt))
+write.table(df_beta_vals_filt,
+             file = "classifying_data/artistic_study_betas_b4_EDMR_50threshold.txt",
+             col.names = TRUE, sep = ";", row.names = TRUE)
 
 
 ## EDMR: calculate all DMRs candidate from complete myDiff dataframe
 print("DMR Analysis next:")
 dm_regions=edmr(myDiff = df_adjusted_diff_meth, mode=2, ACF=TRUE, DMC.qvalue = 0.05, plot = FALSE)
 df_dmrs = data.frame(dm_regions)
-nrow(df_dmrs)
+print(nrow(df_dmrs))
 write.table(df_dmrs,
-            file = "/data/home/bt211038/msc_project/classifying_data/all_DMRs.txt",
-            col.names = TRUE, sep = ";", row.names = TRUE)
-
-
-### remove droplist CpGs
-df_bed_file <- as.data.frame(read.table("/data/home/bt211038/msc_project/bed_file/hg19-blacklist.v2.bed",header = FALSE, sep="\t",stringsAsFactors=FALSE, quote=""))
-colnames(df_bed_file) <- c("chromosome", "start", "end", "info")
-head(df_bed_file)
-df_dmrs_false_cpgs = NULL
-
-for (i in (1:length(df_bed_file$start))) {
-  df_tmp = df_dmrs %>%
-    dplyr::filter(start >= df_bed_file$start[[i]] & end <= df_bed_file$end[[i]] & seqnames != df_bed_file$chromosome[[i]])
-  df_dmrs_false_cpgs = rbind(df_dmrs_false_cpgs, df_tmp)
-}
-
-print(nrow(df_dmrs_false_cpgs))
-
-## retrieve the valid CpG regions
-df_valid_cpg_regions = setdiff(df_dmrs,df_dmrs_false_cpgs)
-print( nrow(df_valid_cpg_regions))
-
-write.table(df_valid_cpg_regions,
-            file = "/data/home/bt211038/msc_project/classifying_data/validated_DMRs_50threshold.txt",
+            file = "classifying_data/all_DMRs.txt",
             col.names = TRUE, sep = ";", row.names = TRUE)
 
 
@@ -143,12 +153,6 @@ print(nrow(df_beta_vals_filtered))
 
 # store filtered beta and m values as TXT ==> will be used to classify data
 write.table(df_beta_vals_filtered,
-            file = "/data/home/bt211038/msc_project/classifying_data/artistic_study_filt-beta-values_0622_50threshold.txt",
+            file = "classifying_data/artistic_study_filt-beta-values_0622_50threshold.txt",
             col.names = TRUE, sep = ";", row.names = TRUE)
  
-# # write.table(df_m_vals_filtered,
-# #             file = "C:/Users/andri/Documents/Uni London/QMUL/SemesterB/Masters_project/msc_project/data/classifying_data/CLL_study_filt-m-values.txt",
-# #             col.names = TRUE, sep = ";", row.names = TRUE)
-# # 
-# # 
-# # # t(df_beta_vals_filtered) %>% as.data.frame() %>% rownames()
