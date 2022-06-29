f24dcf8b6538c4ff60dc824860db4f8ee4bb0299
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-06-29 15:50:11 +0100

    create training, test & validation set + check performance of model on validation set

--- implementation/xgb.py
+++ implementation/xgb.py
@@ -26,7 +26,7 @@ print(df_beta_transposed)
 # try imputing with several imputation methods
 # impute ctrls with ctrls and cases with cases
 # imputer = SimpleImputer(missing_values = np.nan, strategy ='constant', fill_value= 0)
-imputer = SimpleImputer(missing_values = np.nan, strategy ='most_frequent')
+imputer = SimpleImputer(missing_values = np.nan, strategy ='median') # median >> mean >> most_frequent 
 
 # extract and add column with labels (0,1) for control and treated samples
 df_ctrl = df_beta_transposed.loc[lambda x: x['Phenotype'].str.contains(r'(Control)')]
@@ -51,7 +51,7 @@ df.to_csv('./data/classifying_data/ARTISTIC_beta_vals_labelled_data.txt', index=
 
 
 # # Resampling the minority class. The strategy can be changed as required. (source: https://www.analyticsvidhya.com/blog/2021/06/5-techniques-to-handle-imbalanced-data-for-a-classification-problem/)
-# sm = SMOTE(sampling_strategy='minority', random_state=42)
+# sm = SMOTE(sampling_strategy='minority', random_state=20)
 # # Fit the model to generate the data.
 # oversampled_X, oversampled_Y = sm.fit_resample(df.drop('label', axis=1), df['label'])
 # df = pd.concat([pd.DataFrame(oversampled_Y), pd.DataFrame(oversampled_X)], axis=1)
@@ -61,8 +61,14 @@ X = df.drop(['label'], axis=1)
 y = df.loc[:, 'label']
 
 # split data into training and testing data set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=142)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=20)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.75, random_state = 20)
+X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5,  random_state = 20)
+
+print(X_train.shape)
+print(X_test.shape)
+print(X_val.shape)
 
 # ## XGB Hyperparameter Tuning
 # parameters = {'max_depth': [int(x) for x in np.linspace(start = 10, stop = 100, num = 50)],
@@ -71,7 +77,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
 #               'sampling_method': ['uniform', 'gradient_based'],
 #               }
 
-# DTC = DecisionTreeClassifier(random_state = 42, max_features = "auto", class_weight = "balanced", max_depth = None)
+# DTC = DecisionTreeClassifier(random_state = 20, max_features = "auto", class_weight = "balanced", max_depth = None)
 # XGB = xgb.XGBClassifier(base_estimator = DTC)
 
 # # run grid search
@@ -85,15 +91,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_
  
 
 ## train XGB classifier
-clf = xgb.XGBClassifier(learning_rate = 0.08, max_depth = 10, sampling_method = 'uniform', n_estimators=100, random_state=42) # need to adjust according to what the best parameters are
+clf = xgb.XGBClassifier(learning_rate = 0.08, max_depth = 10, sampling_method = 'uniform', n_estimators=100, random_state=20) # need to adjust according to what the best parameters are
 fit = clf.fit(X_train, y_train)
-
-# apply XGB to test data
-# clf = xgb.XGBRFClassifier(n_estimators=100, learning_rate=0.001, max_depth = 100)
-# fit = clf.fit(X_train,y_train)
-
 y_pred = fit.predict(X_test)
 
+print("########## TEST DATA SET ##########")
 # return evaluation metrics
 print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 print("Recall:", metrics.recall_score(y_test, y_pred))
@@ -103,8 +105,8 @@ print("AUC-ROC Score:", metrics.roc_auc_score(y_test, y_pred))
 metrics.RocCurveDisplay.from_estimator(clf, X_test, y_test)
 plt.savefig('./scratch/ROC_xgb_all_features.png')
 # plt.savefig('./figures/ROC_xgb_all_features.png')
+plt.show()
 plt.close()
-# plt.show()
 
 # calculate and plot confusion matrix (source: https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea)
 cf_matrix = metrics.confusion_matrix(y_test, y_pred)
@@ -124,7 +126,7 @@ ax.set_title('Confusion Matrix');
 ax.xaxis.set_ticklabels(['Control', 'Case']); ax.yaxis.set_ticklabels(['Control', 'Case']);
 plt.savefig('./scratch/cf_matrix__xgb_all_features.png')
 # plt.savefig('./figures/cf_matrix__xgb_all_features.png')
-# plt.show()
+plt.show()
 plt.close()
 
 # cf matrix with percentages
@@ -137,6 +139,43 @@ ax.xaxis.set_ticklabels(['Control', 'Case']); ax.yaxis.set_ticklabels(['Control'
 plt.savefig('./scratch/cf_matrix_perc_xgb_all_features.png')
 # plt.savefig('./figures/cf_matrix_perc_xgb_all_features.png')
 plt.close()
+
+
+
+### check performance of model on validation set
+print("########## VALIDATION DATA SET ##########")
+
+y_pred2 = fit.predict(X_val)
+
+# return evaluation metrics
+print("Accuracy:", metrics.accuracy_score(y_val, y_pred2))
+print("Recall:", metrics.recall_score(y_val, y_pred2))
+print("F1 Score:", metrics.f1_score(y_val, y_pred2))
+print("AUC-ROC Score:", metrics.roc_auc_score(y_val, y_pred2))
+
+metrics.RocCurveDisplay.from_estimator(clf, X_val, y_val)
+plt.show()
+plt.close()
+
+# calculate and plot confusion matrix (source: https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea)
+cf_matrix = metrics.confusion_matrix(y_val, y_pred2)
+specificity1 = cf_matrix[0,0]/(cf_matrix[0,0]+cf_matrix[0,1])
+print('Specificity: ', specificity1 )
+
+sensitivity1 = cf_matrix[1,1]/(cf_matrix[1,0]+cf_matrix[1,1])
+print('Sensitivity: ', sensitivity1)
+
+print(metrics.classification_report(y_test, y_pred))
+
+# plot confusion matrix
+ax= plt.subplot()
+sns.heatmap(cf_matrix, annot=True, fmt='.3g', cmap = 'rocket_r')
+ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels'); 
+ax.set_title('Confusion Matrix'); 
+ax.xaxis.set_ticklabels(['Control', 'Case']); ax.yaxis.set_ticklabels(['Control', 'Case']);
+plt.show()
+plt.close()
+
 
 # Feature Selection 
 features = list(df.columns)
@@ -165,15 +204,21 @@ X = df_selected.drop(['label'], axis=1)
 y = df_selected.loc[:, 'label']
 
 # split data into training and testing data set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=142)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.75, random_state=20)
+X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=20)
+
+print(X_train.shape)
+print(X_test.shape)
+print(X_val.shape)
 
 # initialize and train SVM classifier
-clf = xgb.XGBClassifier(learning_rate = 0.08, max_depth = 10, sampling_method = 'uniform', n_estimators=100, random_state=42) # need to adjust according to what the best parameters are
+clf = xgb.XGBClassifier(learning_rate = 0.08, max_depth = 10, sampling_method = 'uniform', n_estimators=100, random_state=20) # need to adjust according to what the best parameters are
 fit = clf.fit(X_train, y_train)
 
 # apply SVM to test data
 y_pred = fit.predict(X_test)
 
+print("########## TEST DATA SET - FEATURE SELECTION ##########")
 # return evaluation metrics
 print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 print("Recall:", metrics.recall_score(y_test, y_pred))
@@ -201,7 +246,7 @@ ax.set_title('Confusion Matrix');
 ax.xaxis.set_ticklabels(['Control', 'Case']); ax.yaxis.set_ticklabels(['Control', 'Case']);
 plt.savefig('./scratch/cf_matrix_xgb_sel_features.png')
 # plt.savefig('./figures/cf_matrix_xgb_sel_features.png')
-# plt.show()
+plt.show()
 plt.close()
 
 # cf matrix with percentages
@@ -213,5 +258,39 @@ ax.set_title('Confusion Matrix');
 ax.xaxis.set_ticklabels(['Control', 'Case']); ax.yaxis.set_ticklabels(['Control', 'Case']);
 plt.savefig('./scratch/cf_matrix_perc_xgb_sel_features.png')
 # plt.savefig('./figures/cf_matrix_perc_xgb_sel_features.png')
+plt.show()
+plt.close()
+
+
+print("########## VALIDATION DATA SET - FEATURE SELECTION ##########")
+
+y_pred2 = fit.predict(X_val)
+
+# return evaluation metrics
+print("Accuracy:", metrics.accuracy_score(y_val, y_pred2))
+print("Recall:", metrics.recall_score(y_val, y_pred2))
+print("F1 Score:", metrics.f1_score(y_val, y_pred2))
+print("AUC-ROC Score:", metrics.roc_auc_score(y_val, y_pred2))
+
+metrics.RocCurveDisplay.from_estimator(clf, X_val, y_val)
 # plt.show()
+plt.close()
+
+# calculate and plot confusion matrix (source: https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea)
+cf_matrix = metrics.confusion_matrix(y_val, y_pred2)
+specificity1 = cf_matrix[0,0]/(cf_matrix[0,0]+cf_matrix[0,1])
+print('Specificity: ', specificity1 )
+
+sensitivity1 = cf_matrix[1,1]/(cf_matrix[1,0]+cf_matrix[1,1])
+print('Sensitivity: ', sensitivity1)
+
+print(metrics.classification_report(y_test, y_pred))
+
+# plot confusion matrix
+ax= plt.subplot()
+sns.heatmap(cf_matrix, annot=True, fmt='.3g', cmap = 'rocket_r')
+ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels'); 
+ax.set_title('Confusion Matrix'); 
+ax.xaxis.set_ticklabels(['Control', 'Case']); ax.yaxis.set_ticklabels(['Control', 'Case']);
+plt.show()
 plt.close()
