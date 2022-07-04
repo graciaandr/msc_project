830583f26c6f81cee057d582f483ad29072d5c3f
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-07-04 14:58:37 +0100

    fixed last things and selected parameters for creating correct beta values for classification

--- implementation/continued_analysis_of_artistic_study.R
+++ implementation/continued_analysis_of_artistic_study.R
@@ -21,7 +21,7 @@ setwd("/data/home/bt211038/msc_project/")
 ## try thresholds of 10%, 25% and 50% for cpgs to keep per conditions
 ## Removing NAs: function to keep rows with n many NAs in that row -- default n is 0.25 
 keep_rows <- function(df, metadata,  perc = 0.25) {
-  samp_per_cpg = round((perc) * 240)
+  samp_per_cpg = round((perc) * 120)
   print(samp_per_cpg)
   col_indeces_Ctrl = which(grepl( "Control" , metadata$Phenotype.RRBS ) )
   col_indeces_Case = which(grepl( "Case" , metadata$Phenotype.RRBS ) )
@@ -51,7 +51,7 @@ print(metadata %>% head(5))
 
 
 calcdiffmeth <- readRDS("artistic_trial/calculateDiffMeth_object.rds")
-df_methDiff = getData(calcdiffmeth)
+df_methDiff = methylKit::getData(calcdiffmeth)
 print("calcdiffmeth has this many rows:")
 print(nrow(calcdiffmeth))
 print(head(calcdiffmeth))
@@ -79,77 +79,200 @@ for (i in (1:length(df_bed_file$start))) {
 }
 
 ## retrieve the valid CpGs
-df_valid_cpgs = setdiff(df_beta_vals,df_dmrs_false_cpgs)
-print( nrow(df_valid_cpgs))
+df_valid_cpgs = setdiff(df_beta_vals, df_dmrs_false_cpgs)
+print(nrow(df_valid_cpgs))
 
-### save valid CpGs and DMCs
+### save valid CpGs
 write.table(df_valid_cpgs,
             file = "classifying_data/df_valid_cpgs.txt",
             col.names = TRUE, sep = ";", row.names = TRUE)
+saveRDS(df_valid_cpgs, file = "df_valid_cpgs.rds")
 
 
 # remove all unnecessary rows
-print("removal of rows with too many NAs (10% threshold) ")
-indeces_to_keep = keep_rows(df = df_valid_cpgs, metadata, perc = 0.1)
-df_beta_vals_filt = df_valid_cpgs[indeces_to_keep,] # clean beta values
+print("removal of rows with too many NAs")
+indeces_to_keep_10 = keep_rows(df = df_valid_cpgs, metadata, perc = 0.1)
+indeces_to_keep_25 = keep_rows(df = df_valid_cpgs, metadata, perc = 0.25)
+indeces_to_keep_50 = keep_rows(df = df_valid_cpgs, metadata, perc = 0.5)
+
+df_beta_vals_filt_10 = df_valid_cpgs[indeces_to_keep_10,] # clean beta values for 10% thrreshold
+df_beta_vals_filt_25 = df_valid_cpgs[indeces_to_keep_25,] # clean beta values for 25% thrreshold
+df_beta_vals_filt_50 = df_valid_cpgs[indeces_to_keep_50,] # clean beta values for 50% thrreshold
 
 print("#Rows of df beta vals after NA handeling: ")
-print(nrow(df_beta_vals_filt))
-write.table(df_beta_vals_filt,
-             file = "classifying_data/artistic_study_betas_b4_EDMR_10threshold_mtDNA.txt",
+print(nrow(df_beta_vals_filt_10))
+print(nrow(df_beta_vals_filt_25))
+print(nrow(df_beta_vals_filt_50))
+
+write.table(df_beta_vals_filt_10,
+             file = "classifying_data/artistic_study_betas_b4_EDMR_10threshold.txt",
              col.names = TRUE, sep = ";", row.names = TRUE)
+saveRDS(df_beta_vals_filt_10, file = "classifying_data/artistic_study_betas_b4_EDMR_10threshold.rds")
+
+write.table(df_beta_vals_filt_25,
+            file = "classifying_data/artistic_study_betas_b4_EDMR_25threshold.txt",
+            col.names = TRUE, sep = ";", row.names = TRUE)
+saveRDS(df_beta_vals_filt_25, file = "classifying_data/artistic_study_betas_b4_EDMR_25threshold.rds")
+
+write.table(df_beta_vals_filt_50,
+            file = "classifying_data/artistic_study_betas_b4_EDMR_50threshold.txt",
+            col.names = TRUE, sep = ";", row.names = TRUE)
+saveRDS(df_beta_vals_filt_50, file = "classifying_data/artistic_study_betas_b4_EDMR_50threshold.rds")
+
 
 ### filter calcdiffmeth so that only keeps rows from cleaned beta values
-calcdiffmeth2 = NULL
-# for (i in (1:length(df_beta_vals$start))) {
-for (i in (1:500)) { # to check if it works
-  df_tmp = df_methDiff %>%
-    dplyr::filter(start <= df_beta_vals$pos[[i]] & end >= df_beta_vals$pos[[i]] & chr == df_beta_vals$chrom[[i]])
-  calcdiffmeth2 = rbind(calcdiffmeth2, df_tmp)
+calcdiffmeth2_10 = NULL
+calcdiffmeth2_25 = NULL
+calcdiffmeth2_50 = NULL
+df_tmp_10 = NULL
+df_tmp_25 = NULL
+df_tmp_50 = NULL
+
+for (i in (1:nrow(df_beta_vals_filt_10))) {
+  df_tmp_10 = df_methDiff %>%
+    dplyr::filter(start <= df_beta_vals_filt_10$pos[[i]] & end >= df_beta_vals_filt_10$pos[[i]] & chr == df_beta_vals_filt_10$chrom[[i]])
+  calcdiffmeth2_10 = rbind(calcdiffmeth2_10, df_tmp_10)
 }
 
-calcdiffmeth2 = calcdiffmeth2 %>% distinct()
-nrow(calcdiffmeth2)
+for (i in (1:nrow(df_beta_vals_filt_25))) {
+  df_tmp_25 = df_methDiff %>%
+    dplyr::filter(start <= df_beta_vals_filt_25$pos[[i]] & end >= df_beta_vals_filt_25$pos[[i]] & chr == df_beta_vals_filt_25$chrom[[i]])
+  calcdiffmeth2_25 = rbind(calcdiffmeth2_25, df_tmp_25)
+}
+
+for (i in (1:nrow(df_beta_vals_filt_50))) {
+  df_tmp_50 = df_methDiff %>%
+    dplyr::filter(start <= df_beta_vals_filt_50$pos[[i]] & end >= df_beta_vals_filt_50$pos[[i]] & chr == df_beta_vals_filt_50$chrom[[i]])
+  calcdiffmeth2_50 = rbind(calcdiffmeth2_50, df_tmp_50)
+}
+
+calcdiffmeth2_10 = calcdiffmeth2_10 %>% distinct()
+print("calcdiffmeth2 has these dimensions (10%):")
+print(nrow(calcdiffmeth2_10))
 
 ## Q Value adjustment of filtered calc Meth Diff Object
-calcdiffmeth2$qvalue = p.adjust(calcdiffmeth2$pvalue, method = "BH")
+calcdiffmeth2_10$qvalue = p.adjust(calcdiffmeth2_10$pvalue, method = "BH")
+saveRDS(calcdiffmeth2_10, file = "classifying_data/calcdiffmeth2_10.rds")
+
+calcdiffmeth2_25$qvalue = p.adjust(calcdiffmeth2_25$pvalue, method = "BH")
+saveRDS(calcdiffmeth2_25, file = "classifying_data/calcdiffmeth2_25.rds")
+
+calcdiffmeth2_50$qvalue = p.adjust(calcdiffmeth2_50$pvalue, method = "BH")
+saveRDS(calcdiffmeth2_50, file = "classifying_data/calcdiffmeth2_50.rds")
 
 # filter calcdiffmeth for qvalue of 0.05 and methylationDifference of 10 
 # df_adjusted_diff_meth = methylKit::getMethylDiff(qvalue = 0.05, difference = 10)
-df_DMPs = calcdiffmeth2 %>% filter(qvalue < 0.05 & abs(meth.diff) >= 10)
+df_DMPs_10 = calcdiffmeth2_10 %>% filter(qvalue < 0.05 & abs(meth.diff) >= 10)
+df_DMPs_25 = calcdiffmeth2_25 %>% filter(qvalue < 0.05 & abs(meth.diff) >= 10)
+df_DMPs_50 = calcdiffmeth2_50 %>% filter(qvalue < 0.05 & abs(meth.diff) >= 10)
 
-print("df_DMPs has these dimensions:")
-print(dim(df_DMPs))
-write.table(df_DMPs,
-            file = "classifying_data/df_adjusted_diff_meth.txt",
+print("df_DMPs has these dimensions (10%):")
+print(dim(df_DMPs_10))
+
+write.table(df_DMPs_10,
+            file = "classifying_data/df_DMPs_10.txt",
             col.names = TRUE, sep = ";", row.names = TRUE)
+saveRDS(df_DMPs_10, file = "classifying_data/df_DMPs_10.rds")
+
+write.table(df_DMPs_25,
+            file = "classifying_data/df_DMPs_25",
+            col.names = TRUE, sep = ";", row.names = TRUE)
+saveRDS(df_DMPs_25, file = "classifying_data/df_DMPs_25.rds")
+
+write.table(df_DMPs_50,
+            file = "classifying_data/df_DMPs_50.txt",
+            col.names = TRUE, sep = ";", row.names = TRUE)
+saveRDS(df_DMPs_50, file = "classifying_data/df_DMPs_50.rds")
 
 
 ## for loop that goes through the start pos, end pos, and seqnames per row in beta/m value dataframe and DMR data
 ## to retrieve CpG sites that are DMPs
-df_tmp1 = data.frame(matrix(NA, nrow = 1, ncol = ncol(df_beta_vals_filt)))
-colnames(df_tmp1) <- colnames((df_beta_vals_filt))
-df_beta_vals_filtered = NULL
-for (i in (1:length(df_DMPs$start))) {
-  df_tmp1 = df_beta_vals_filt %>%
-    dplyr::filter(pos >= df_DMPs$start[[i]] & pos <= df_DMPs$end[[i]] & chrom == df_DMPs$chr[[i]])
-  df_beta_vals_filtered = rbind(df_beta_vals_filtered, df_tmp1)
+df_tmp10 = data.frame(matrix(NA, nrow = 1, ncol = ncol(df_beta_vals_filt_10)))
+df_tmp25 = data.frame(matrix(NA, nrow = 1, ncol = ncol(df_beta_vals_filt_25)))
+df_tmp50 = data.frame(matrix(NA, nrow = 1, ncol = ncol(df_beta_vals_filt_50)))
+
+colnames(df_tmp10) <- colnames((df_beta_vals_filt_10))
+colnames(df_tmp25) <- colnames((df_beta_vals_filt_25))
+colnames(df_tmp50) <- colnames((df_beta_vals_filt_50))
+
+df_beta_vals_filtered_10 = NULL
+df_beta_vals_filtered_25 = NULL
+df_beta_vals_filtered_50 = NULL
+
+for (i in (1:length(df_DMPs_10$start))) {
+  df_tmp10 = df_beta_vals_filt_10 %>%
+    dplyr::filter(pos >= df_DMPs_10$start[[i]] & pos <= df_DMPs_10$end[[i]] & chrom == df_DMPs_10$chr[[i]])
+  df_beta_vals_filtered_10 = rbind(df_beta_vals_filtered_10, df_tmp10)
 }
+print(head(df_beta_vals_filtered_10))
+print(nrow(df_beta_vals_filtered_10))
 
-print(head(df_beta_vals_filtered))
-print(nrow(df_beta_vals_filtered))
+for (i in (1:length(df_DMPs_25$start))) {
+  df_tmp25 = df_beta_vals_filt_25 %>%
+    dplyr::filter(pos >= df_DMPs_25$start[[i]] & pos <= df_DMPs_25$end[[i]] & chrom == df_DMPs_25$chr[[i]])
+  df_beta_vals_filtered_25 = rbind(df_beta_vals_filtered_25, df_tmp25)
+}
+print(head(df_beta_vals_filtered_25))
+print(nrow(df_beta_vals_filtered_25))
+
+for (i in (1:length(df_DMPs_50$start))) {
+  df_tmp50 = df_beta_vals_filt_50 %>%
+    dplyr::filter(pos >= df_DMPs_50$start[[i]] & pos <= df_DMPs_50$end[[i]] & chrom == df_DMPs_50$chr[[i]])
+  df_beta_vals_filtered_50 = rbind(df_beta_vals_filtered_50, df_tmp50)
+}
+print(head(df_beta_vals_filtered_50))
+print(nrow(df_beta_vals_filtered_50))
+
+col_indeces_Ctrl = which(grepl( "Control" , metadata$Phenotype.RRBS ) )
+col_indeces_Case = which(grepl( "Case" , metadata$Phenotype.RRBS ) )
+
+df_ctrl_10 <- df_beta_vals_filtered_10[ , col_indeces_Ctrl ]
+df_ctrl_10[nrow(df_ctrl_10) + 1,] <- paste0("Control", (1:120))
+df_cases_10 <- df_beta_vals_filtered_10[ , col_indeces_Case ]
+df_cases_10[nrow(df_cases_10) + 1,] <- paste0("Case", (1:120))
+
+df_ctrl_25 <- df_beta_vals_filtered_25[ , col_indeces_Ctrl ]
+df_ctrl_25[nrow(df_ctrl_25) + 1,] <- paste0("Control", (1:120))
+df_cases_25 <- df_beta_vals_filtered_25[ , col_indeces_Case ]
+df_cases_25[nrow(df_cases_25) + 1,] <- paste0("Case", (1:120))
+
+df_ctrl_50 <- df_beta_vals_filtered_50[ , col_indeces_Ctrl ]
+df_ctrl_50[nrow(df_ctrl_50) + 1,] <- paste0("Control", (1:120))
+df_cases_50 <- df_beta_vals_filtered_50[ , col_indeces_Case ]
+df_cases_50[nrow(df_cases_50) + 1,] <- paste0("Case", (1:120))
+
+df_beta_vals_filtered_10 <- cbind(df_ctrl_10, df_cases_10)
+rownames(df_beta_vals_filtered_10)[nrow(df_beta_vals_filtered_10)] <- "Phenotype"
+
+df_beta_vals_filtered_50 <- cbind(df_ctrl_25, df_cases_25)
+rownames(df_beta_vals_filtered_25)[nrow(df_beta_vals_filtered_50)] <- "Phenotype"
+
+df_beta_vals_filtered_50 <- cbind(df_ctrl_50, df_cases_50)
+rownames(df_beta_vals_filtered_50)[nrow(df_beta_vals_filtered_50)] <- "Phenotype"
+
 # store filtered beta and m values as TXT ==> will be used to classify data
-write.table(df_beta_vals_filtered,
-            file = "classifying_data/artistic_study_filt-beta-values_0622_10threshold_mtDNA.txt",
+write.table(df_beta_vals_filtered_10,
+            file = "classifying_data/artistic_study_filt-beta-values_0722_10threshold.txt",
+            col.names = TRUE, sep = ";", row.names = TRUE)
+write.table(df_beta_vals_filtered_25,
+            file = "classifying_data/artistic_study_filt-beta-values_0722_25threshold.txt",
+            col.names = TRUE, sep = ";", row.names = TRUE)
+write.table(df_beta_vals_filtered_50,
+            file = "classifying_data/artistic_study_filt-beta-values_0722_50threshold.txt",
             col.names = TRUE, sep = ";", row.names = TRUE)
 
-## EDMR: calculate all DMRs candidate from complete calcdiffmeth dataframe
-print("DMR Analysis:")
-dm_regions=edmr(myDiff = calcdiffmeth2, mode=2, ACF=TRUE, DMC.qvalue = 0.05, plot = FALSE)
-df_dmrs = data.frame(dm_regions)
-print(nrow(df_dmrs))
-write.table(df_dmrs,
-            file = "classifying_data/DMRs.txt",
-            col.names = TRUE, sep = ";", row.names = TRUE)
+saveRDS(df_beta_vals_filtered_10, file = "classifying_data/df_beta_vals_filtered_10.rds")
+saveRDS(df_beta_vals_filtered_25, file = "classifying_data/df_beta_vals_filtered_25.rds")
+saveRDS(df_beta_vals_filtered_50, file = "classifying_data/df_beta_vals_filtered_50.rds")
+
+# ## EDMR: calculate all DMRs candidate from complete calcdiffmeth dataframe
+# print("DMR Analysis:")
+# dm_regions=edmr(myDiff = calcdiffmeth2, mode=2, ACF=TRUE, DMC.qvalue = 0.05, plot = FALSE)
+# df_dmrs = data.frame(dm_regions)
+# print(nrow(df_dmrs))
+# write.table(df_dmrs,
+#             file = "classifying_data/DMRs.txt",
+#             col.names = TRUE, sep = ";", row.names = TRUE)
+# saveRDS(df_dmrs, file = "classifying_data/DMRs.rds")
 
  
