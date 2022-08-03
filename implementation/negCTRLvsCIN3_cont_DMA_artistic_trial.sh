cafaba37fdacb55d39fc3692fc235a7e4b58da89
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-08-03 17:21:10 +0100

    Create negCTRLvsCIN3_cont_DMA_artistic_trial.sh

---
+++ implementation/negCTRLvsCIN3_cont_DMA_artistic_trial.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -pe smp 8 
+#$ -l h_rt=240:0:0
+#$ -l h_vmem=32G
+#$ -l highmem
+#$ -N part2_negCTRLvsCIN3
+#$ -m bea
+
+# load R
+module load R/3.6.1
+
+# run R script that did DMA for artistic study 
+Rscript negCTRLvsCIN3_continued_analysis_of_artistic_study.R
