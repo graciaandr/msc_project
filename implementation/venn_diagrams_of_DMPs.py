094c8b5e9db2b01223d08b1f4f7ed7e9b788305f
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-08-05 15:18:27 +0100

    Update venn_diagrams_of_DMPs.py

--- implementation/venn_diagrams_of_DMPs.py
+++ implementation/venn_diagrams_of_DMPs.py
@@ -1,33 +1,60 @@
 import matplotlib.pyplot as plt
-from matplotlib_venn import venn2
+from matplotlib_venn import venn2, venn3, venn3_circles
 import pandas as pd
 import numpy as np
 
+# CTRL vs CIN2+/CIN2/CIN3
+RF_features_CTRLvsCIN2plus = pd.read_csv('./data/classifying_data/RF_features_CTRLvsCIN2+.csv', sep = ';')
+SVM_features_CTRLvsCIN2 = pd.read_csv('./data/classifying_data/SVM_features_CTRLvsCIN2.csv', sep = ';', index_col=False)
+SVM_features_CTRLvsCIN3 = pd.read_csv('./data/classifying_data/SVM_features_CTRLvsCIN3.csv', sep = ';')
 
-# load data sets
+# neg CTRL vs CIN2+/CIN2/CIN3
+RF_features_negCTRLvsCIN2plus = pd.read_csv('./data/classifying_data/RF_features_negCTRLvsCIN2+.csv', sep = ';')
+SVM_features_negCTRLvsCIN2 = pd.read_csv('./data/classifying_data/SVM_features_negCTRLvsCIN2.csv', sep = ';')
+GradBoost_features_negCTRLvsCIN3 = pd.read_csv('./data/classifying_data/GradBoost_features_negCTRLvsCIN3.csv', sep = ';', index_col=False)
 
-# all controls vs CIN2+ 
-CTRLvsCIN2plus_artistic_study_filt = pd.read_csv('./data/classifying_data/artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
-# CTRLvsCIN2plus_artistic_study_filt = pd.read_csv('./classifying_data/artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
+col_one_list = RF_features_CTRLvsCIN2plus['RF_features'].to_list()
+col_two_list = SVM_features_CTRLvsCIN2['SVM_features'].to_list()
+col_three_list = SVM_features_CTRLvsCIN3['SVM_features'].to_list()
 
-# all controls vs CIN2
-CTRLvsCIN2_artistic_study_filt = pd.read_csv('./data/classifying_data/CTRLvsCIN2_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
-# CTRLvsCIN2_artistic_study_filt = pd.read_csv('./classifying_data/CTRLvsCIN2_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
+col_four_list = RF_features_negCTRLvsCIN2plus['RF_features'].to_list()
+col_five_list = SVM_features_negCTRLvsCIN2['SVM_features'].to_list()
+col_six_list = GradBoost_features_negCTRLvsCIN3['GradBoost_features'].to_list()
 
-# all controls vs CIN3
-CTRLvsCIN3_artistic_study_filt = pd.read_csv('./data/classifying_data/CTRLvsCIN3_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
-# CTRLvsCIN3_artistic_study_filt = pd.read_csv('./classifying_data/CTRLvsCIN3_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
+set1 = set(col_one_list)
+set2 = set(col_two_list)
+set3 = set(col_three_list)
+set4 = set(col_four_list)
+set5 = set(col_five_list)
+set6 = set(col_six_list)
 
+# venn2([set1, set2], ("CTRL vs CIN2+", "CTRL vs CIN2"))
+# plt.show()
 
-# negative controls vs CIN2+
-negCTRL_CIN2plus_artistic_study_filt = pd.read_csv('./data/classifying_data/negCTRL_CIN2+_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
-# negCTRL_CIN2plus_artistic_study_filt = pd.read_csv('./classifying_data/negCTRL_CIN2+_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
+# venn2([set1, set3], ("CTRL vs CIN2+", "CTRL vs CIN3"))
+# plt.show()
 
-# negative controls vs CIN2
-negCTRLvsCIN2_artistic_study_filt = pd.read_csv('./data/classifying_data/negCTRLvsCIN2_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
-# negCTRLvsCIN2_artistic_study_filt = pd.read_csv('./classifying_data/negCTRLvsCIN2_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
+# venn2([set2, set3], ("CTRL vs CIN2", "CTRL vs CIN3"))
+# plt.show()
 
-# negative controls vs CIN3
-negCTRLvsCIN3_artistic_study_filt = pd.read_csv('./data/classifying_data/negCTRLvsCIN3_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
-# negCTRLvsCIN3_artistic_study_filt = pd.read_csv('./classifying_data/negCTRLvsCIN3_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
+# venn3([set1, set2, set3], ("CTRL vs CIN2+", "CTRL vs CIN2", "CTRL vs CIN3"))
+# plt.savefig('./figures/venn_3comp_CTRL.png')
+# plt.show()
 
+# print(set4)
+# print(set5)
+vd = venn3([set4, set5, set6], ("neg CTRL vs CIN2+", "neg CTRL vs CIN2", "neg CTRL vs CIN3"))
+
+lbl = vd.get_label_by_id("A")
+x, y = lbl.get_position()
+lbl.set_position((x, y+0.81))  
+
+lbl = vd.get_label_by_id("C")
+x, y = lbl.get_position()
+lbl.set_position((x+0.1, y-0.1))  
+
+lbl = vd.get_label_by_id("B")
+x, y = lbl.get_position()
+lbl.set_position((x-0.1, y-0.1))  
+plt.savefig('./figures/venn_3comp_negCTRL.png')
+plt.show()
