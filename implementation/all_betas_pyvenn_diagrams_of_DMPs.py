4644f544d6e7dcaae6586eb3da49dababf70240c
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-08-08 12:07:32 +0100

    Update all_betas_pyvenn_diagrams_of_DMPs.py

--- implementation/all_betas_pyvenn_diagrams_of_DMPs.py
+++ implementation/all_betas_pyvenn_diagrams_of_DMPs.py
@@ -5,35 +5,36 @@ import pandas as pd
 import numpy as np
 
 # CTRL vs CIN2+/CIN2/CIN3
-RF_features_CTRLvsCIN2plus = pd.read_csv('./data/classifying_data/RF_features_CTRLvsCIN2+.csv', sep = ';')
-SVM_features_CTRLvsCIN2 = pd.read_csv('./data/classifying_data/SVM_features_CTRLvsCIN2.csv', sep = ';', index_col=False)
-SVM_features_CTRLvsCIN3 = pd.read_csv('./data/classifying_data/SVM_features_CTRLvsCIN3.csv', sep = ';')
+CTRLvsCIN2plus = pd.read_csv('./data/classifying_data/artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
+CTRLvsCIN2 = pd.read_csv('./data/classifying_data/CTRLvsCIN2_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
+CTRLvsCIN3 = pd.read_csv('./data/classifying_data/CTRLvsCIN3_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
+print(CTRLvsCIN3.head(5))
 
 # neg CTRL vs CIN2+/CIN2/CIN3
-RF_features_negCTRLvsCIN2plus = pd.read_csv('./data/classifying_data/RF_features_negCTRLvsCIN2+.csv', sep = ';')
-SVM_features_negCTRLvsCIN2 = pd.read_csv('./data/classifying_data/SVM_features_negCTRLvsCIN2.csv', sep = ';')
-GradBoost_features_negCTRLvsCIN3 = pd.read_csv('./data/classifying_data/GradBoost_features_negCTRLvsCIN3.csv', sep = ';', index_col=False)
+negCTRLvsCIN2plus = pd.read_csv('./data/classifying_data/negCTRL_CIN2+_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
+negCTRLvsCIN2 = pd.read_csv('./data/classifying_data/negCTRLvsCIN2_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
+negCTRLvsCIN3 = pd.read_csv('./data/classifying_data/negCTRLvsCIN3_artistic_study_filt-beta-values_0722_50threshold.txt', sep = ';')
 
-RF_features_CTRLvsCIN2plus = RF_features_CTRLvsCIN2plus['RF_features'].to_list()
-SVM_features_CTRLvsCIN2 = SVM_features_CTRLvsCIN2['SVM_features'].to_list()
-SVM_features_CTRLvsCIN3 = SVM_features_CTRLvsCIN3['SVM_features'].to_list()
+CTRLvsCIN2plus = list(CTRLvsCIN2plus.index)
+CTRLvsCIN2 = list(CTRLvsCIN2.index)
+CTRLvsCIN3 = list(CTRLvsCIN3.index)
 
-RF_features_negCTRLvsCIN2plus = RF_features_negCTRLvsCIN2plus['RF_features'].to_list()
-SVM_features_negCTRLvsCIN2 = SVM_features_negCTRLvsCIN2['SVM_features'].to_list()
-GradBoost_features_negCTRLvsCIN3 = GradBoost_features_negCTRLvsCIN3['GradBoost_features'].to_list()
+negCTRLvsCIN2plus = list(negCTRLvsCIN2plus.index)
+negCTRLvsCIN2 = list(negCTRLvsCIN2.index)
+negCTRLvsCIN3 = list(negCTRLvsCIN3.index)
 
 Cin2plusComparisons = {
-    "CTRL vs CIN2+": set(RF_features_CTRLvsCIN2plus),
-    "neg CTRL vs CIN2+": set(RF_features_negCTRLvsCIN2plus),
+    "CTRL vs CIN2+": set(CTRLvsCIN2plus),
+    "neg CTRL vs CIN2+": set(negCTRLvsCIN2plus),
 }
 venn(Cin2plusComparisons, cmap="plasma")
 plt.show()
 
 negCtrls_comparisons_dict = {
-    "CTRL vs CIN2": set(SVM_features_CTRLvsCIN2),
-    "CTRL vs CIN3": set(SVM_features_CTRLvsCIN3),
-    "neg CTRL vs CIN2": set(SVM_features_negCTRLvsCIN2),
-    "neg CTRL vs CIN3": set(GradBoost_features_negCTRLvsCIN3), 
+    "CTRL vs CIN2": set(CTRLvsCIN2),
+    "CTRL vs CIN3": set(CTRLvsCIN3),
+    "neg CTRL vs CIN2": set(negCTRLvsCIN2),
+    "neg CTRL vs CIN3": set(negCTRLvsCIN3), 
 }
 
 venn(negCtrls_comparisons_dict, cmap="plasma")
@@ -41,16 +42,16 @@ plt.show()
 
 
 ctrlCin2ctrlCin2plus = {
-    "CTRL vs CIN2+": set(RF_features_CTRLvsCIN2plus),
-    "CTRL vs CIN2": set(SVM_features_CTRLvsCIN2),
+    "CTRL vs CIN2+": set(CTRLvsCIN2plus),
+    "CTRL vs CIN2": set(CTRLvsCIN2),
 }
 
 # venn(ctrlCin2ctrlCin2plus, cmap="plasma")
 # plt.show()
 
 ctrlCin3ctrlCin2plus = {
-    "CTRL vs CIN2+": set(RF_features_CTRLvsCIN2plus),
-    "CTRL vs CIN3": set(SVM_features_CTRLvsCIN3),
+    "CTRL vs CIN2+": set(CTRLvsCIN2plus),
+    "CTRL vs CIN3": set(CTRLvsCIN3),
 }
 
 # venn(ctrlCin3ctrlCin2plus, cmap="plasma")
@@ -58,68 +59,68 @@ ctrlCin3ctrlCin2plus = {
 
 
 ctrlCin3ctrlCin2 = {
-    "CTRL vs CIN2": set(SVM_features_CTRLvsCIN2),
-    "CTRL vs CIN3": set(SVM_features_CTRLvsCIN3),
+    "CTRL vs CIN2": set(CTRLvsCIN2),
+    "CTRL vs CIN3": set(CTRLvsCIN3),
 }
 
-# venn(ctrlCin3ctrlCin2, cmap="plasma")
-# plt.show()
+venn(ctrlCin3ctrlCin2, cmap="plasma")
+plt.show()
 
 allctrl_comparisons_dict = {
-    "CTRL vs CIN2+": set(RF_features_CTRLvsCIN2plus),
-    "CTRL vs CIN2": set(SVM_features_CTRLvsCIN2),
-    "CTRL vs CIN3": set(SVM_features_CTRLvsCIN3),
+    "CTRL vs CIN2+": set(CTRLvsCIN2plus),
+    "CTRL vs CIN2": set(CTRLvsCIN2),
+    "CTRL vs CIN3": set(CTRLvsCIN3),
 }
 
-# venn(allctrl_comparisons_dict, cmap="viridis", fontsize=20, alpha=0.425)
-# plt.savefig('./figures/VennDiagram_allCTRLsvsCIN2pCIN2CIN3.png')
+venn(allctrl_comparisons_dict, cmap="viridis", fontsize=20, alpha=0.425)
+plt.savefig('./figures/VennDiagram_allCTRLsvsCIN2pCIN2CIN3_AllFeatures.png')
 # plt.show()
 
 negCtrls_comparisons_dict = {
-    "neg CTRL vs CIN2+": set(RF_features_negCTRLvsCIN2plus),
-    "neg CTRL vs CIN2": set(SVM_features_negCTRLvsCIN2),
+    "neg CTRL vs CIN2+": set(negCTRLvsCIN2plus),
+    "neg CTRL vs CIN2": set(negCTRLvsCIN2),
 }
 
 # venn(negCtrls_comparisons_dict, cmap="plasma")
 # plt.show()
 
 negCtrls_comparisons_dict = {
-    "neg CTRL vs CIN2+": set(RF_features_negCTRLvsCIN2plus),
-    "neg CTRL vs CIN3": set(GradBoost_features_negCTRLvsCIN3), 
+    "neg CTRL vs CIN2+": set(negCTRLvsCIN2plus),
+    "neg CTRL vs CIN3": set(negCTRLvsCIN3), 
 }
 
 # venn(negCtrls_comparisons_dict, cmap="plasma")
 # plt.show()
 
 negCtrls_comparisons_dict = {
-    "neg CTRL vs CIN2": set(SVM_features_negCTRLvsCIN2),
-    "neg CTRL vs CIN3": set(GradBoost_features_negCTRLvsCIN3), 
+    "neg CTRL vs CIN2": set(negCTRLvsCIN2),
+    "neg CTRL vs CIN3": set(negCTRLvsCIN3), 
 }
 
 # venn(negCtrls_comparisons_dict, cmap="plasma")
 # plt.show()
 
 negCtrls_comparisons_dict = {
-    "neg CTRL vs CIN2+": set(RF_features_negCTRLvsCIN2plus),
-    "neg CTRL vs CIN2": set(SVM_features_negCTRLvsCIN2),
-    "neg CTRL vs CIN3": set(GradBoost_features_negCTRLvsCIN3), 
+    "neg CTRL vs CIN2+": set(negCTRLvsCIN2plus),
+    "neg CTRL vs CIN2": set(negCTRLvsCIN2),
+    "neg CTRL vs CIN3": set(negCTRLvsCIN3), 
 }
 
-venn(negCtrls_comparisons_dict, cmap="plasma",  fontsize=22, alpha=0.425)
-plt.savefig('./figures/VennDiagram_allnegCTRLsvsCIN2pCIN2CIN3.png')
+venn(negCtrls_comparisons_dict, cmap="plasma",  fontsize=20, alpha=0.425)
+plt.savefig('./figures/VennDiagram_allnegCTRLsvsCIN2pCIN2CIN3_AllFeatures.png')
 plt.show()
 
 all6_comparisons_dict = {
-    "CTRL vs CIN2+": set(RF_features_CTRLvsCIN2plus),
-    "CTRL vs CIN2": set(SVM_features_CTRLvsCIN2),
-    "CTRL vs CIN3": set(SVM_features_CTRLvsCIN3),
-    "neg CTRL vs CIN2+": set(RF_features_negCTRLvsCIN2plus),
-    "neg CTRL vs CIN2": set(SVM_features_negCTRLvsCIN2),
-    "neg CTRL vs CIN3": set(GradBoost_features_negCTRLvsCIN3), 
+    "CTRL vs CIN2+": set(CTRLvsCIN2plus),
+    "CTRL vs CIN2": set(CTRLvsCIN2),
+    "CTRL vs CIN3": set(CTRLvsCIN3),
+    "neg CTRL vs CIN2+": set(negCTRLvsCIN2plus),
+    "neg CTRL vs CIN2": set(negCTRLvsCIN2),
+    "neg CTRL vs CIN3": set(negCTRLvsCIN3), 
 }
 
 
 pseudovenn(all6_comparisons_dict, cmap="plasma")
-plt.savefig('./figures/VennDiagram_all6comparisons.png')
+plt.savefig('./figures/VennDiagram_all6comparisons_AllFeatures.png')
 plt.show()
 
