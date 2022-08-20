ef0c9b7a890dc0c59120e64ffcb9cfcb513c57a9
Gracia Andriamiadana <61698737+graciaandr@users.noreply.github.com>
2022-08-20 17:02:33 +0100

    reduced the opacity of colors in barcharts by 25 %

--- implementation/barplots_ggplot2.R
+++ implementation/barplots_ggplot2.R
@@ -14,8 +14,8 @@ df = df %>% mutate(color = ColorsDT$Color)
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO,
                                    y = CTRLvsCIN2Plus)) +
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=CTRLvsCIN2Plus), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=CTRLvsCIN2Plus), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
@@ -27,8 +27,8 @@ ggsave("figures/sensitivity_barplot_CTRLvsCIN2Plus.png",
        bg = "white", width = 10, height = 5)
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO, y = FS_CTRLvsCIN2Plus)) +
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=FS_CTRLvsCIN2Plus), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=FS_CTRLvsCIN2Plus), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
@@ -40,8 +40,8 @@ ggsave("figures/sensitivity_barplot_FS_CTRLvsCIN2Plus.png",
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO,
                                    y = CTRLvsCIN2)) +
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=CTRLvsCIN2), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=CTRLvsCIN2), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
@@ -53,8 +53,8 @@ ggsave("figures/sensitivity_barplot_CTRLvsCIN2.png",
        bg = "white", width = 10, height = 5)
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO, y = FS_CTRLvsCIN2)) +
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=FS_CTRLvsCIN2), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=FS_CTRLvsCIN2), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
@@ -65,8 +65,8 @@ ggsave("figures/sensitivity_barplot_FS_CTRLvsCIN2.png",
        bg = "white", width = 10, height = 5)
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO, y = CTRLvsCIN3Plus)) +
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=CTRLvsCIN3Plus), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=CTRLvsCIN3Plus), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
@@ -78,8 +78,8 @@ ggsave("figures/sensitivity_barplot_CTRLvsCIN3Plus.png",
 
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO, y = FS_CTRLvsCIN3Plus)) +
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=FS_CTRLvsCIN3Plus), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=FS_CTRLvsCIN3Plus), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
@@ -91,8 +91,8 @@ ggsave("figures/sensitivity_barplot_FS_CTRLvsCIN3Plus.png",
 
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO, y = negCTRLvsCIN2Plus)) +
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=negCTRLvsCIN2Plus), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=negCTRLvsCIN2Plus), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
@@ -104,8 +104,8 @@ ggsave("figures/sensitivity_barplot_negCTRLvsCIN2Plus.png",
 
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO, y = FS_negCTRLvsCIN2Plus)) +
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=FS_negCTRLvsCIN2Plus), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=FS_negCTRLvsCIN2Plus), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
@@ -117,8 +117,8 @@ ggsave("figures/sensitivity_barplot_FS_negCTRLvsCIN2Plus.png",
 
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO, y = negCTRLvsCIN2)) + 
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=negCTRLvsCIN2), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=negCTRLvsCIN2), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
@@ -130,8 +130,8 @@ ggsave("figures/sensitivity_barplot_negCTRLvsCIN2.png",
 
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO, y = FS_negCTRLvsCIN2)) + 
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=FS_negCTRLvsCIN2), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=FS_negCTRLvsCIN2), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
@@ -143,8 +143,8 @@ ggsave("figures/sensitivity_barplot_FS_negCTRLvsCIN2.png",
 
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO, y = negCTRLvsCIN3Plus)) + 
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=negCTRLvsCIN3Plus), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=negCTRLvsCIN3Plus), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
@@ -156,8 +156,8 @@ ggsave("figures/sensitivity_barplot_negCTRLvsCIN3Plus.png",
 
 
 p = ggplot2::ggplot(data = df, aes(x = ALGO, y = FS_negCTRLvsCIN3Plus)) + 
-  geom_bar(stat="identity", color = df$color, fill = df$color) +
-  geom_text(aes(label=FS_negCTRLvsCIN3Plus), vjust=1.6, color="white", size=10) +
+  geom_bar(stat="identity", color = df$color, fill = df$color, alpha = 0.75) +
+  geom_text(aes(label=FS_negCTRLvsCIN3Plus), vjust=1.6, color="white", size=12) +
   theme_minimal() + theme(axis.title.x = element_text(size = 20),
                           axis.text.x = element_text(size = 18),
                           axis.title.y = element_text(size = 20),
